d9229ec32f7375c8827c97de63442f00a40900b1
Neeraj Premchand <117623953+NeerajP666@users.noreply.github.com>
2025-10-03 00:32:43 +0530

    Day 8

---
+++ Day 8/UserServiceWCF/UserServiceWCF/DataAccessBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace UserServiceWCF
+{
+    public abstract class DataAccessBase
+    {
+        protected string connectionString =
+            ConfigurationManager.ConnectionStrings["mydbconnection"].ConnectionString;
+
+        
+        protected delegate T ExecuteCommand<T>(SqlCommand cmd);
+
+       
+        protected T Execute<T>(string query, SqlParameter[] parameters, ExecuteCommand<T> execMethod)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+
+                con.Open();
+                return execMethod(cmd);
+            }
+        }
+    }
+}
---
+++ Day 8/UserServiceWCF/UserServiceWCF/Service1.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+
+
+namespace UserServiceWCF
+{
+    public class Service1 : IService1
+    {
+        private readonly UserDAL dal = new UserDAL();
+
+        public bool Register(User user)
+        {
+            return dal.Register(user.username, user.password, user.email);
+        }
+
+        public bool Login(User user)
+        {
+            return dal.Login(user.username, user.password);
+
+        }
+        public int GetNextUserID()
+        {
+            UserDAL dal = new UserDAL();
+            return dal.GetNextUserID();
+        }
+    }
+}
