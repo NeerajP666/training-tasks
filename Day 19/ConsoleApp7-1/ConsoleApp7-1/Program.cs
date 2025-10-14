b2c2cff323b84ad8f8a8a43451f6ae70390c19f6
Neeraj Premchand <117623953+NeerajP666@users.noreply.github.com>
2025-10-14 19:41:34 +0530

    Day 19

---
+++ Day 19/ConsoleApp7-1/ConsoleApp7-1/Program.cs
@@ -0,0 +1,51 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.ComponentModel.DataAnnotations;
+class Student
+{
+    public string Name {  get; set; }
+    public int Age { get; set; }
+    public string Grade { get; set; }
+
+    public Student(string name, int age, string grade)
+    {
+        Name = name;
+        Age = age;
+        Grade = grade;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"name ={Name}");
+        Console.WriteLine($"age={Age}");
+        Console.WriteLine($"grade={Grade}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        List<Student> list = new List<Student>();
+        Console.WriteLine("how many students do you wan to add");
+        int a=Convert.ToInt32(Console.ReadLine());
+        for(int i=1;i<=a;i++)
+        {
+            Console.WriteLine($"enter details of student {i}");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Grade: ");
+            string grade = Console.ReadLine();
+            Student s = new Student(name, age, grade);
+            list.Add(s);
+
+        }
+        Console.WriteLine("datas you entered are");
+        foreach(Student s in  list)
+        {
+            s.Display();
+        }
+    }
+}
---
+++ Day 19/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,52 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Simple Calculator ===");
+
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Choose an operation: +  -  *  /");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                {
+                    Console.WriteLine("Cannot divide by zero!");
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid operation!");
+                return;
+        }
+
+        Console.WriteLine($"Result: {result}");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
