756fb707ffccffe1936bf5d1553d623138167d57
Neeraj Premchand <117623953+NeerajP666@users.noreply.github.com>
2025-10-16 18:34:12 +0530

    Day 21

---
+++ Day 21/ProductApi/ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models;
+using ProductApi.Services;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly ProductService _service;
+
+        public ProductsController(ProductService service) 
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.GetAll());
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var product = _service.GetById(id);
+            if (product == null) 
+                return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _service.Add(product);
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product updatedProduct)
+        {
+            var updated = _service.Update(id, updatedProduct);
+            if (!updated) 
+                return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var deleted = _service.Delete(id);
+            if (!deleted) 
+                return NotFound(); 
+            return NoContent();
+        }
+    }
+}
---
+++ Day 21/ProductApi/ProductApi/Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using ProductApi.Models;
+
+namespace ProductApi.Services
+{
+    public class ProductService
+    {
+        private readonly List<Product> _products = new List<Product>();
+
+        public List<Product> GetAll() => _products;
+
+        public Product GetById(int id) => _products.FirstOrDefault(p => p.Id == id);
+
+        public void Add(Product product)
+        {
+            if (_products.Any(p => p.Id == product.Id))
+                throw new Exception("Product with this ID already exists");
+
+            _products.Add(product);
+        }
+
+        public bool Update(int id, Product updatedProduct)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                product.Name = updatedProduct.Name;
+                product.Price = updatedProduct.Price;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Delete(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                _products.Remove(product);
+                return true; 
+            }
+            return false;
+        }
+    }
+}
