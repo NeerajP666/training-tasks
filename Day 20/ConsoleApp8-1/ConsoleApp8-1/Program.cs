e61617169095c54c87c4acc4c274f23d151960c9
Neeraj Premchand <117623953+NeerajP666@users.noreply.github.com>
2025-10-16 18:32:40 +0530

    Day 20

---
+++ Day 20/ConsoleApp8-1/ConsoleApp8-1/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8_1
+{
+    
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {Id}, Name: {Name}");
+        }
+    }
+
+    public class AdminUser : User
+    {
+        public string Role { get; set; }
+
+        public AdminUser(int id, string name, string role)
+            : base(id, name)
+        {
+            Role = role;
+        }
+
+       
+        public void DisplayAdminInfo()
+        {
+            DisplayInfo();
+            Console.WriteLine($"Role: {Role}");
+        }
+    }
+
+    public interface IUserService
+    {
+        void AddUser();
+        void ViewUsers();
+        void DeleteUser();
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly List<User> _users = new();
+
+        public void AddUser()
+        {
+            Console.Write("Enter ID: ");
+            int id = int.Parse(Console.ReadLine()!);
+
+            if (_users.Exists(u => u.Id == id))
+            {
+                Console.WriteLine("User with this ID already exists.\n");
+                return;
+            }
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine()!;
+
+            Console.Write("Is this user an Admin? (y/n): ");
+            string ans = Console.ReadLine()!.ToLower();
+
+            User user;
+            if (ans == "y")
+            {
+                Console.Write("Enter Role: ");
+                string role = Console.ReadLine()!;
+                user = new AdminUser(id, name, role);
+            }
+            else
+            {
+                user = new User(id, name);
+            }
+
+            _users.Add(user);
+            Console.WriteLine("User added!\n");
+        }
+
+        public void ViewUsers()
+        {
+            if (_users.Count == 0)
+            {
+                Console.WriteLine("No users found.\n");
+                return;
+            }
+
+            foreach (var user in _users)
+            {
+                if (user is AdminUser admin)
+                    admin.DisplayAdminInfo();
+                else
+                    user.DisplayInfo();
+            }
+
+            Console.WriteLine();
+        }
+
+        public void DeleteUser()
+        {
+            Console.Write("Enter ID to delete: ");
+            int id = int.Parse(Console.ReadLine()!);
+
+            var user = _users.Find(u => u.Id == id);
+            if (user != null)
+            {
+                _users.Remove(user);
+                Console.WriteLine("User deleted!\n");
+            }
+            else
+            {
+                Console.WriteLine("User not found!\n");
+            }
+        }
+    }
+    public class UserController
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service) => _service = service;
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("1: Add User  2: View Users  3: Delete User  4: Exit");
+                Console.Write("Choice: ");
+                int choice = int.Parse(Console.ReadLine()!);
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1: _service.AddUser(); 
+                        break;
+                    case 2: _service.ViewUsers(); 
+                        break;
+                    case 3: _service.DeleteUser(); 
+                        break;
+                    case 4: return;
+                    default: Console.WriteLine("Invalid choice\n"); break;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            IUserService service = new UserService(); 
+            var controller = new UserController(service);
+            controller.Run();
+        }
+    }
+}
---
+++ Day 20/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,60 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+    class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Cannot divide by zero!");
+                return 0;
+            }
+            return a / b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            while (true)
+            {
+                Console.Write("Enter first number: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Choose operation (+, -, *, /): ");
+                char op = Convert.ToChar(Console.ReadLine());
+                double result = 0;                
+                switch (op)
+                {
+                    case '+': result = calc.Add(num1, num2); break;
+                    case '-': result = calc.Subtract(num1, num2); break;
+                    case '*': result = calc.Multiply(num1, num2); break;
+                    case '/': result = calc.Divide(num1, num2); break;
+                    default:
+                        Console.WriteLine("Invalid operator!");
+                        continue;
+                }
+                Console.WriteLine($"Result: {result}\n");
+                Console.Write("Do you want to continue? (y/n) ");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() != "y")
+                    break;
+            }
+        }
+    }
